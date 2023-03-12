663f87c025ecc7714850c944715d71f5ae3697ed
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-03-12 18:58:33 +0900

    최종 끝

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
-
     private final MemberRepository memberRepository;
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
